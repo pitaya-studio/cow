0d84757ae964f57f3bdcd03fd18c6e96d71eb71f
YingXia <191502003@qq.com>
2014-07-10 20:38:56 +0800

    日产奶量界面
    日产奶量输入界面

---
+++ CowSite/Controllers/Milk/DailyMilkYieldController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CowSite.Controllers.Milk
+{
+    public class DailyMilkYieldController : Controller
+    {
+        //
+        // GET: /DailyMilkYield/
+        public ActionResult Add() 
+        {
+            return View("~/Views/Milk/DailyMilkYield/Add.cshtml");
+        }
+    }
+}

04ca5abaab3c3ea12fc44cceb522c4f8ba51a782
YingXia <191502003@qq.com>
2014-07-12 10:12:55 +0800

    DailyMilkYield
    日产奶量功能

--- CowSite/Controllers/Milk/DailyMilkYieldController.cs
+++ CowSite/Controllers/Milk/DailyMilkYieldController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DairyCow.BLL;
+using DairyCow.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,11 +10,21 @@ namespace CowSite.Controllers.Milk
 {
     public class DailyMilkYieldController : Controller
     {
+        MilkRecordBLL bllMilkRecord = new MilkRecordBLL();
         //
         // GET: /DailyMilkYield/
-        public ActionResult Add() 
+        public ActionResult Add()
         {
             return View("~/Views/Milk/DailyMilkYield/Add.cshtml");
+        }
+
+        public ActionResult Save()
+        {
+            MilkSale milkSale = new MilkSale();
+            UpdateModel<MilkSale>(milkSale);
+            bllMilkRecord.InsertMilkSale(milkSale);
+            //有几个字段未赋值
+            return RedirectToAction("../Index/List");
         }
     }
 }

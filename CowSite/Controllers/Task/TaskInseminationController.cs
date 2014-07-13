6752d4765d69d65af4b677d0c5f01e22814e588d
baodehua <baodehua@live.cn>
2014-07-13 15:59:51 +0800

    任务单页面示例

---
+++ CowSite/Controllers/Task/TaskInseminationController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace CowSite.Controllers.Task
+{
+    /// <summary>
+    /// 发情配种任务处理
+    /// </summary>
+    public class TaskInseminationController : Controller
+    {
+        public JsonResult LoadTask(string taskID)
+        {
+            return Json(new { Instruction = "请观测牛群发情状况，并给发情牛配种。", EarNum = "11111" }, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult SaveTask()
+        {
+            string op = Request.Form["operatorName"];
+
+            return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}

f01292a824a808dcd6f772fd58ac27d89a617164
archerlw <btree@163.com>
2014-07-13 20:52:09 +0800

    产后15天任务单

---
+++ CowSite/Controllers/Task/TaskDay15AfterBornController.cs
@@ -0,0 +1,43 @@
+﻿using DairyCow.BLL;
+using DairyCow.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CowSite.Controllers.Task
+{
+    public class TaskDay15AfterBornController : Controller
+    {
+        public class TaskDay21ToBornController : Controller
+        {
+            public JsonResult LoadTask(string taskID)
+            {
+                TaskBLL bll = new TaskBLL();
+                DairyTask v;
+                v = bll.GetTaskByID(Convert.ToInt32(taskID));
+
+                UserBLL u = new UserBLL();
+                User user = u.GetUsers().Find(p => p.ID == v.OperatorID);
+
+                return Json(new
+                {
+                    EarNum = v.EarNum,
+                    ArrivalTime = v.ArrivalTime.ToString("yyyy-MM-dd"),
+                    Operator = user.Name
+                }, JsonRequestBehavior.AllowGet);
+            }
+
+            public JsonResult SaveTask()
+            {
+                TaskBLL bll = new TaskBLL();
+                DairyTask v = bll.GetTaskByID(Convert.ToInt32(Request.Form["id"]));
+                v.CompleteTime = DateTime.Parse(Request.Form["endDate"]);
+
+                bll.CompleteDay15AfterBorn(v);
+                return Json(new { status = 0 }, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}

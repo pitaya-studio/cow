e01f7f8d7448638ff956b329748e3dd54fad0144
YingXia <191502003@qq.com>
2014-07-13 18:39:44 +0800

    免疫
    增加免疫后台功能

--- CowSite/Controllers/Task/TaskCompleteImmuneController.cs
+++ CowSite/Controllers/Task/TaskCompleteImmuneController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DairyCow.BLL;
+using DairyCow.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,14 +15,30 @@ namespace CowSite.Controllers.Task
         /// </summary>
         /// <param name="taskID"></param>
         /// <returns></returns>
-        public JsonResult LoadTask(string taskID)
+        TaskBLL bllTask = new TaskBLL();
+        public JsonResult LoadTask()
         {
-            return Json("");
+            var taskData = new { doctor = "ss" };
+            return Json(taskData, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult SaveTask()
+        public ActionResult SaveTask(string id)
         {
+            Immune immune = new Immune();
+            int PastureID = UserBLL.Instance.CurrentUser.Pasture.ID;
+            string ImmuneDate = Request.Form["immuneDate"];
+            string Vaccine = Request.Form["immuneType"];
+            string EarNum = Request.Form["earNum"];
+            string DoctorID = id;
 
+            immune.PastureID = Convert.ToInt32(PastureID);
+            immune.ImmuneDate = Convert.ToDateTime(ImmuneDate);
+            immune.Vaccine = Vaccine;
+            immune.EarNum = Convert.ToInt32(EarNum);
+            immune.DoctorID = Convert.ToInt32(DoctorID);
+
+            //增加一头的免疫记录
+            bllTask.AddImmuneRecord();
             return View("~/Views/Task/Index.cshtml");
         }
     }

a70f35facc023510e6fc004aa0f9df4901883998
archerlw <btree@163.com>
2014-07-13 19:14:30 +0800

    hahha

--- BLL/TaskBLL.cs
+++ BLL/TaskBLL.cs
@@ -295,7 +295,7 @@ namespace DairyCow.BLL
         /// <summary>
         /// 产前7天任务
         /// </summary>
-        public void CompleteDay7ToBorn(DairyTask t)
+        public void CompleteDay7ToBorn(DairyTask t, int cowHouseId, int cowGroupId)
         {
             //此任务单在，产犊界面/事件中产生，或者流产早产；流产等会取消之前的未完成产前任务单
             //更新任务记录，标记完成

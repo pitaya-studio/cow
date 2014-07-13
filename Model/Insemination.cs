a976fb6b3f8e1303cfc0acd883c8ec057799c868
YingXia <191502003@qq.com>
2014-07-13 17:42:36 +0800

    增加OperatorID
    增加OperatorID

--- Model/Insemination.cs
+++ Model/Insemination.cs
@@ -15,7 +15,7 @@ namespace DairyCow.Model
         public int ID { get; set; }
         //耳号
         public int EarNum { get; set; }
-        //配次
+        //配次（改为冻精数）
         public int InseminationNum { get; set; }
         //冻精编号
         public string SemenNum { get; set; }
@@ -27,6 +27,8 @@ namespace DairyCow.Model
         public DateTime OperateDate { get; set; }
         //配种员
         public string Operator { get; set; }
+        //配种员ID
+        public int operatorID { get; set; }
         //描述
         public string Description { get; set; }
         //发情日期

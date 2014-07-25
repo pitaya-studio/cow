255f187fe8692a6f4fb5331d44e57ac7168a4021
sopyzhu <sopy.zhu@139.com>
2014-07-25 17:26:35 +0800

    康复，未调通

--- CowSite/Controllers/Medical/AdjustController.cs
+++ CowSite/Controllers/Medical/AdjustController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using DairyCow.BLL;
+using DairyCow.Model;
 
 namespace CowSite.Controllers.Medical
 {
@@ -14,6 +16,31 @@ namespace CowSite.Controllers.Medical
         public ActionResult Index()
         {
             return View("~/Views/CowGroup/Adjust.cshtml");
+        }
+
+        public JsonResult GetSickCows()
+        {
+            CowBLL cBLL = new CowBLL();
+            List<Cow> list = cBLL.GetCowList().FindAll(p=>p.IsIll);
+            return Json(list, JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult RecoverCows(string cowID)
+        {
+            CowBLL cBLL = new CowBLL();
+            int earN=Int32.Parse(cowID);
+            Cow myCow = cBLL.GetCowInfo(earN);
+            int temp=cBLL.UpdateCowIllStatus(earN, false);
+            if (temp==1)
+            {
+                //success
+                return Json(new {Result=1},JsonRequestBehavior.AllowGet);
+            }
+            else
+            {
+                return Json(new {Result=0},JsonRequestBehavior.AllowGet);
+            }
+
         }
 	}
 }

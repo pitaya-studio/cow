0cc82f67bd65b569ec17be30ec495cad2ab59b0a
YingXia <191502003@qq.com>
2014-07-27 15:44:04 +0800

    增加干奶
    干奶

---
+++ CowSite/Controllers/Feed/DryMilkController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CowSite.Controllers.Feed
+{
+    /// <summary>
+    /// 干奶
+    /// </summary>
+    public class DryMilkController : Controller
+    {
+        //
+        // GET: /DryMilk/
+        public ActionResult Add()
+        {
+            return View("~/Views/Feed/DryMilk/Add.cshtml");
+        }
+    }
+}

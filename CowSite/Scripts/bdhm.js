01676274f8120c7486ad8c3a3a9b76bdaf369da0
baodehua <baodehua@live.cn>
2014-11-30 10:59:50 +0800

    bd

---
+++ CowSite/Scripts/bdhm.js
@@ -0,0 +1,2 @@
+﻿var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3F769db917e1b91a6d48bf2884edce9df7' type='text/javascript'%3E%3C/script%3E"));
